329b1d183bb4926eea120a0c65ffca097bf78abc
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-24 09:46:15 +0530

    Update help.py

--- beta/utils/inline/help.py
+++ beta/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from beta import app
+from Beta import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
